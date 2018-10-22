45a2d3d4004e19901512b26871a43af0393430ef
ghk2300@gmail.com <>
2018-10-22 15:22:27 -0400

    This is the file commit

---
+++ src/githubsetup/GitHubSetup.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubsetup;
+
+/**
+ *
+ * @author ghk23
+ */
+public class GitHubSetup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lodge();
+    }
+    
+    public static void Lodge(){
+        System.out.println("Jonathon Lodge!!!");
+    }
+    
+}
